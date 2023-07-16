2c2a55b7a55fa2afc2859e4282f8d37f93246115
Phumin Decoknoi <139684701+PhuminDecOKnoi@users.noreply.github.com>
2023-07-16 17:55:53 +0700

    Create get_register.php

---
+++ get_register.php
@@ -0,0 +1,25 @@
+<?php
+
+    include("function.php"); # การนำไฟล์ฟังก์ชันมาใช้งาน (Function.php)
+    $_err = "";
+    $_get_title_page = "Get Register Page"; # การกำหนดชื่อเพจ
+    set_header($_get_title_page); # การรับค่า HTML header มาใช้งาน
+
+    $_user_name = $_POST["user_name"];
+    $_password = $_POST["password"];
+    $_re_password = $_POST["re_password"];
+    $_age = $_POST["age"];
+    $_gender = $_POST["gender"];
+    $_mobile = $_POST["mobile"];
+
+    if(empty($_user_name)|| empty($_password)|| empty($_re_password)|| empty($_age)|| empty($_gender)|| empty($_mobile)){  
+        redirect_register_page();
+    }else{
+        if($_password != $_re_password){
+            $_err = "Password not incorrect";
+            echo alert_err($_err).link_backToRegister();
+        }
+    }
+
+    set_footer(); # การรับค่า HTML footer มาใช้งาน
+?>
