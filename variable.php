9903d92d7c2048e5a0cabcc48ca4ebc33f98e3df
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-16 20:12:06 +0700

    Update variable.php

--- variable.php
+++ variable.php
@@ -1,7 +1,18 @@
 <?php
+    include("functions.php"); # การนำไฟล์ฟังก์ชันมาใช้งาน (Function.php)
+
+    $_get_title_page = "Variable"; # การกำหนดชื่อเพจ
+    set_header($_get_title_page); # การรับค่า HTML header มาใช้งาน
 
     $url = "http://www.githup.com/phumindecoknoi";
     $status = false;
-    if(isset($status) && $status !- true)
+    if(isset($status) && $status !== true)
         echo $url;
+
+    $a = null;
+    $b = 0;
+
+    if($a == $b) echo "เกิดข้อผิดพลาด" ;
+
+    include("footer.php");
 ?>
